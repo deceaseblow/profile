bd84f4b2f9e5cb8b0deed3c04d4f88145efa1dff
Sabina Huseynzada <sebine.huseyn0@gmail.com>
2025-09-26 00:13:21 +0400

    all good

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,33 +47,35 @@ export default function Home() {
             ) : (
               <p className="text-gray-400 italic">No socials available.</p>
             )}
+          </div> 
+          
+          <div>
+          <h2 className="text-center text-black font-bold tracking-wider text-[25px] md:text-start">
+            LIST OF THINGS TO KEEP IN MIND
+          </h2>
+          <div>
+            {data?.list && data.list.length > 0 ? (
+              <ul className="list-disc list-inside space-y-2 pl-5 text-[16px] md:text-[18px] tracking-wide text-gray-800 mt-2">
+                {data.list.map((item, index) => (
+                  <li
+                    key={index}
+                    className="hover:text-black hover:translate-x-1 transition-all duration-200 text-[20px]"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-gray-400 text-center italic mt-2 md:text-start">
+                No list available yet.
+              </p>
+            )}
           </div>
         </div>
+        </div>
 
-        {/* <div>
-            <h2 className="text-center text-black font-bold tracking-wider text-[25px] md:text-start">
-              LIST OF THINGS TO KEEP IN MIND
-            </h2>
-            <div>
-              {data?.list && data.list.length > 0 ? (
-                <ul className="list-disc list-inside space-y-2 pl-5 text-[16px] md:text-[18px] tracking-wide text-gray-800 mt-2">
-                  {data.list.map((item, index) => (
-                    <li
-                      key={index}
-                      className="hover:text-black hover:translate-x-1 transition-all duration-200 text-[20px]"
-                    >
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="text-gray-400 text-center italic mt-2 md:text-start">
-                  No list available yet.
-                </p>
-              )}
-            </div>
-          </div> 
-          */}
+       
+
         {/* 
           <div className="flex flex-col items-center md:items-start">
             <h2 className="text-center text-black font-bold tracking-wider text-[25px] md:text-start">
